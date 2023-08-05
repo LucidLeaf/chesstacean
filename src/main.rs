67132859a838fb2ab300d5e605412799a00511f0
Leo Schultheiss <leo.schultheiss@tum.de>
2023-08-05 19:30:19 +0200

    extract game loop from main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use crate::board_state::{BoardState, PositionVector, BLACK, WHITE};
 use std::io;
+use std::process::exit;
+
+use crate::board_state::{BoardState, Position};
 
 mod board_state;
 
-fn notation_to_coordinates(input: String) -> Result<PositionVector, &'static str> {
+fn notation_to_coordinates(input: String) -> Result<Position, &'static str> {
     if input.len() != 2 {
         return Err("Invalid input length");
     }
@@ -17,21 +19,21 @@ fn notation_to_coordinates(input: String) -> Result<PositionVector, &'static str
         return Err("Invalid row");
     }
     let col = match column_string {
-        "a" => 0,
-        "b" => 1,
-        "c" => 2,
-        "d" => 3,
-        "e" => 4,
-        "f" => 5,
-        "g" => 6,
-        "h" => 7,
+        "a" => 1,
+        "b" => 2,
+        "c" => 3,
+        "d" => 4,
+        "e" => 5,
+        "f" => 6,
+        "g" => 7,
+        "h" => 8,
         _ => 0,
     };
     let row: i32 = row_string.parse().expect("Not a number");
-    return Ok(PositionVector { row: row - 1, col });
+    return Ok(Position { row: row - 1, col: col - 1 });
 }
 
-fn coordinates_to_notation(input: PositionVector) -> String {
+fn coordinates_to_notation(input: Position) -> String {
     let row = char::from_digit((input.row + 1) as u32, 10).expect("Invalid coordinate");
     let col: char = char::from(97u8 + input.col as u8);
     let mut result = col.to_string();
@@ -44,37 +46,52 @@ fn read_line(prompt: &str) -> String {
     println!("{}", prompt);
     io::stdin().read_line(&mut input).expect("No input received");
     input = input.replace("\n", "");
+    if input == "exit" {
+        exit(0);
+    }
     return input;
 }
 
-fn main() {
+fn game_loop() {
     let mut bs: BoardState = BoardState::new();
     loop {
         println!("{}", bs.str());
-        println!("white in check: {}\nblack in check: {}", bs.is_color_in_check(WHITE), bs.is_color_in_check(BLACK));
-        let input = read_line("Provide coordinates of piece to move:");
-        let coordinates = match notation_to_coordinates(input) {
+        println!("white in check: {}\nblack in check: {}", bs.is_color_in_check(board_state::WHITE), bs.is_color_in_check(board_state::BLACK));
+        let input = read_line("Provide coordinates of piece:");
+        let original_square = match notation_to_coordinates(input) {
             Ok(c) => c,
             Err(e) => {
                 println!("{}", e);
                 continue;
             }
         };
-        let moves = bs.get_piece_moves_respecting_checks(coordinates);
+        let moves = bs.get_piece_moves_respecting_checks(original_square);
         if moves.len() == 0 {
             println!("no possible moves");
             continue;
         }
         for i in 0..moves.len() {
-            println!("[{0}] {1}", i, coordinates_to_notation(coordinates + moves[i]));
-        }
-        let move_number: usize = loop {
-            let number: usize = read_line("select move (0, 1...)").parse().expect("Not a number");
-            if number < moves.len() {
-                break number;
+            print!("{}", coordinates_to_notation(original_square + moves[i]));
+            if i < moves.len() - 1 {
+                print!(" ,");
+            } else {
+                println!()
             }
-            println!("Invalid number");
+        }
+        let square_to_move_to: Position = loop {
+            let written_move: String = read_line("select move:");
+            let position = notation_to_coordinates(written_move);
+            if position.is_err() {
+                println!("Not a move from above");
+            } else {
+                break position.unwrap();
+            }
         };
-        bs = bs.perform_move(coordinates, moves[move_number]);
+        let relative_move = Position::absolute_to_relative(original_square, square_to_move_to);
+        bs = bs.perform_move(original_square, relative_move);
     }
+}
+
+fn main() {
+    game_loop();
 }
